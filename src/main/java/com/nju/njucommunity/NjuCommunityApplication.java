b09d50a3e3c94b343a286661dce0c072e27b7f4f
Rashawn <ruichenxue@smail.nju.edu.cn>
2023-11-06 19:44:40 +0800

    init repo

---
+++ src/main/java/com/nju/njucommunity/NjuCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.nju.njucommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjuCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NjuCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nju/njucommunity/NjuCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.njucommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjuCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
